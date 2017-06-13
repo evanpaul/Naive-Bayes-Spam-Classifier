09c1e93b5065f8cf2ee44edd7f49dfb20773197f
evanpaul22 <evanpaul22@gmail.com>
2017-06-13 13:00:45 -0700

    Add CL arguments; Prepare for analyze source merging

--- test_model.py
+++ test_model.py
@@ -6,6 +6,8 @@ from sklearn.feature_selection import mutual_info_classif, SelectKBest
 from sklearn.neighbors import KNeighborsClassifier
 import os.path
 import argparse
+import sys
+
 
 def predict(data, model, spam_flag):
     tries = 0
@@ -46,10 +48,50 @@ def test_model(ham, spam, model):
     print "=" * 50
 
 
+def load_data(spam_target, ham_target):
+    spam_data = None
+    ham_data = None
+
+    spam_check = spam_target + "0.csv"
+    ham_check = ham_target + "0.csv"
+    # Check first split for existence
+    if not os.path.isfile(spam_check) or not os.path.isfile(ham_check):
+        sys.exit("Data not found!")
+
+    for i in range(NUM_SPLITS):
+        spam_path = spam_target + str(i) + ".csv"
+        ham_path = ham_target + str(i) + ".csv"
+
+        if os.path.isfile(spam_path):
+            spam_data_part = pd.read_csv(spam_path, sep=',', engine='c',
+                                         header=None, na_filter=False, dtype=np.int16, low_memory=False)
+            print "=>", spam_path
+
+            if spam_data is None:
+                spam_data = spam_data_part.values
+            else:
+                spam_data = np.concatenate((spam_data, spam_data_part.values))
+
+        if os.path.isfile(ham_path):
+            ham_data_part = pd.read_csv(ham_path, sep=',', engine='c',
+                                        header=None, na_filter=False, dtype=np.int16, low_memory=False)
+            print "=>", ham_path
+
+            if ham_data is None:
+                ham_data = ham_data_part.values
+            else:
+                ham_data = np.concatenate((ham_data, ham_data_part.values))
+    return spam_data, ham_data
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--score-features', dest="score", action='store_true')
-    parser.add_argument('--trec', dest="trec", action='store_true')
+    parser.add_argument('-f', '--reduce-features', dest="features", type=int,
+                        help="number of features to reduce to using mutual information scoring")
+    parser.add_argument('-e', '--enron', dest="enron", action='store_true',
+                        help="use Enron dataset")
+    parser.add_argument('-t', '--trec', dest="trec", action='store_true',
+                        help="use TREC07 dataset")
     args = parser.parse_args()
 
     DATA_path = "data/"
@@ -58,47 +100,40 @@ if __name__ == "__main__":
     GNB = GaussianNB()
     BNB = BernoulliNB()
 
-    NUM_SPLITS = 10
+    NUM_SPLITS = 11
 
-
-    print "Loading data..."
     # Pandas is super efficient at loading CSVs
-    spam_data = None
-    ham_data = None
-    for i in range(NUM_SPLITS):
-
-        if args.trec:
-            spam_target = DATA_path + 'trec_spam_output' + str(i) + ".csv"
-            ham_target = DATA_path + 'trec_ham_output' + str(i) + ".csv"
-        else:
-            spam_target = DATA_path + 'enron_spam_output' + str(i) + ".csv"
-            ham_target = DATA_path + 'enron_ham_output' + str(i) + ".csv"
-
-
-        if os.path.isfile(spam_target):
-            spam_data_part = pd.read_csv(spam_target, sep=',', engine='c',
-                                    header=None, na_filter=False, dtype=np.int16, low_memory=False)
-            print "=>", spam_target
-
-            if spam_data is None:
-                spam_data = spam_data_part.values
-            else:
-                spam_data = np.concatenate((spam_data, spam_data_part.values))
-        if os.path.isfile(ham_target):
-            ham_data_part = pd.read_csv(ham_target, sep=',', engine='c',
-                                    header=None, na_filter=False, dtype=np.int16, low_memory=False)
-            print "=>", ham_target
-
-            if ham_data is None:
-                ham_data = ham_data_part.values
-            else:
-                ham_data = np.concatenate((ham_data, ham_data_part.values))
-
-
+    if (args.trec and args.enron) or (not args.trec and not args.enron):
+        # If neither dataset is specified, use both
+        # print "Loading TREC and Enron datasets..."
+        # trec_spam_target = DATA_path + 'trec_spam_output'
+        # trec_ham_target = DATA_path + 'trec_ham_output'
+        # trec_spam_data, trec_ham_data = load_data(
+        #     trec_spam_target, trec_ham_target)
+        #
+        # enron_spam_target = DATA_path + 'enron_spam_output'
+        # enron_ham_target = DATA_path + 'enron_ham_output'
+        # enron_spam_data, enron_ham_data = load_data(
+        #     enron_spam_target, enron_ham_target)
+        #
+        # spam_data = np.concatenate((trec_spam_data, enron_spam_data))
+        # ham_data = np.concatenate((trec_ham_data, enron_ham_data))
+        sys.exit("Specify exactly one dataset!")
+        # TODO Merge analyze source files to make features match!
+    elif args.trec:
+        print "Loading TREC dataset..."
+        spam_target = DATA_path + 'trec_spam_output'
+        ham_target = DATA_path + 'trec_ham_output'
+        spam_data, ham_data = load_data(spam_target, ham_target)
+    elif args.enron:
+        print "Loading Enron dataset..."
+        spam_target = DATA_path + 'enron_spam_output'
+        ham_target = DATA_path + 'enron_ham_output'
+        spam_data, ham_data = load_data(spam_target, ham_target)
+    else:
+        sys.exit("Invalid arguments!")
 
     # Convert dataframe to numpy format
-    # spam_data = spam_data.values
-    # ham_data = ham_data.values
     ham_label = np.zeros(shape=len(ham_data))
     spam_label = np.ones(shape=len(spam_data))
 
@@ -109,14 +144,14 @@ if __name__ == "__main__":
     testing_ham = ham_data[int(.8 * len(ham_data)):]
 
     # Remove features based on MI score
-    if args.score:
-        print "%d features before filter" %len(training_ham[0])
+    if args.features:
+        print "%d features before filter" % len(training_ham[0])
         print "Selecting features based on mutual information score..."
 
-        num_features = 2000
-        sel = SelectKBest(score_func=mutual_info_classif, k=num_features)
+        sel = SelectKBest(score_func=mutual_info_classif, k=args.features)
         x = np.concatenate((training_ham, training_spam))
-        y = np.concatenate((ham_label[:len(training_ham)], spam_label[:len(training_spam)]))
+        y = np.concatenate(
+            (ham_label[:len(training_ham)], spam_label[:len(training_spam)]))
         sel = sel.fit(x, y)
 
         features = sel.get_support()
@@ -130,8 +165,6 @@ if __name__ == "__main__":
         training_ham = np.delete(training_ham, bad_features, axis=1)
         testing_spam = np.delete(testing_spam, bad_features, axis=1)
         testing_ham = np.delete(testing_ham, bad_features, axis=1)
-
-
 
     print "Using %d features for classification..." % len(training_spam[0])
 
@@ -152,20 +185,9 @@ if __name__ == "__main__":
     BNB = BNB.fit(combined_training_data, combined_training_labels)
     test_model(training_ham, training_spam, BNB)
     test_model(testing_ham, testing_spam, BNB)
-    # print "K NEAREST NEIGHBORS (k=%d)" % k
-    # try:
-    #     KNN = KNN.fit(combined_training_data, combined_training_labels)
-    #     test_model(training_ham, training_spam, KNN)
-    #     test_model(testing_ham, testing_spam, KNN)
-    # except ValueError:
-    #     print "KNN failed!"
-    #     print "%d training instances with %d features" % (len(combined_training_data), len(combined_training_data[0]))
-    #     print "%d training labels" % len(combined_training_labels)
-    #     print combined_training_data.shape
-    #     print combined_training_labels.shape
-
 
 #
 # For enron, trec, and combined:
     # Observe accuracies for Naive Bayes variations while varying number of features (might have to remove pruning in analyze_corpus.py)
-    # Observe KNN accuracy at different neighbor counts at ideal feature count found above
+    # Observe KNN accuracy at different neighbor counts at ideal feature count
+    # found above
