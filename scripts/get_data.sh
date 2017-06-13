6c3c8d09759af04dc5fe051b7c67c4f9537f181c
evanpaul22 <evanpaul22@gmail.com>
2017-06-12 22:04:56 -0700

    Fix get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Run these in the cd
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron1.tar.gz -P ../enron
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron2.tar.gz -P ../enron
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron3.tar.gz -P ../enron
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron4.tar.gz -P ../enron
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron5.tar.gz -P ../enron
-wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron6.tar.gz -P ../enron
+# Run these from the scripts directory!
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron1.tar.gz -P ../enron/
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron2.tar.gz -P ../enron/
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron3.tar.gz -P ../enron/
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron4.tar.gz -P ../enron/
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron5.tar.gz -P ../enron/
+wget http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron6.tar.gz -P ../enron/
 
-tar -xvf ../enron/enron1.tar.gz
-tar -xvf ../enron/enron2.tar.gz
-tar -xvf ../enron/enron3.tar.gz
-tar -xvf ../enron/enron4.tar.gz
-tar -xvf ../enron/enron5.tar.gz
-tar -xvf ../enron/enron6.tar.gz
+tar -xvf ../enron/enron1.tar.gz -C ../enron/
+tar -xvf ../enron/enron2.tar.gz -C ../enron/
+tar -xvf ../enron/enron3.tar.gz -C ../enron/
+tar -xvf ../enron/enron4.tar.gz -C ../enron/
+tar -xvf ../enron/enron5.tar.gz -C ../enron/
+tar -xvf ../enron/enron6.tar.gz -C ../enron/
 
-wget http://plg.uwaterloo.ca/cgi-bin/cgiwrap/gvcormac/trec07p.tgz -P ../
-tar -xzvf trec07p.tgz
+# Something is up with this archive... have to SFTP the file over I guess
+# wget http://plg.uwaterloo.ca/cgi-bin/cgiwrap/gvcormac/trec07p.tgz -P ../
+# tar -xzvf trec07p.tgz
