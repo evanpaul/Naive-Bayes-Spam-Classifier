362f6a2446505214e1f96ce6e33587f7a7d07aa2
evanpaul22 <evanpaul22@gmail.com>
2017-06-14 14:52:28 -0700

    Add softer threshold to word counts

--- project_utils.py
+++ project_utils.py
@@ -49,12 +49,12 @@ def remove_insignificant(master_word_dict):
         sys.exit("Empty master_word_dict! Did you forget to downloaded the datasets?")
     remove = []
     for word_key in sorted(master_word_dict):
-        if master_word_dict[word_key] < avg:
+        if master_word_dict[word_key] < avg/4:
             remove.append(word_key)
     for r in remove:
         del master_word_dict[r]
 
-    print "Reduced %d features to %d features using threshold of %d" % (before, len(master_word_dict), avg)
+    print "Reduced %d features to %d features using threshold of count>%d" % (before, len(master_word_dict), avg/2)
 
 # Remove "non-decisive" entries
 # def remove_ambiguous(ham_counts, spam_counts, master_word_dict, threshold=0.58):
